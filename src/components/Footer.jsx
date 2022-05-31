90d56db4544c13e15e6291d6e65c054ad5627655
Martin Mwangi <mwangimartin1904@gmail.com>
2022-05-31 20:47:13 +0300

    ✨ Updates on charts, cart modal, notification...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
-const Footer = () => {
-  return (
-	<div>Footer</div>
-  )
-}
+const Footer = () => (
+    <div className="mt-24">
+        <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+            © {new Date().getFullYear()} All rights reserved. By {" "}
+            <a
+                href="https://www.fiverr.com/martin_devs/create-node-js-reactjs-application"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="underline"
+            >
+                Martin Mwangi
+            </a>
+        </p>
+    </div>
+);
 
-export default Footer
+export default Footer;
